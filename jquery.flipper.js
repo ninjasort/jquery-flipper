539618416001758937cd72806af41e7e927e91dd
Cameron J Roe <cameronjroe@gmail.com>
2014-08-31 19:29:41 -0400

    update demo

--- jquery.flipper.js
+++ jquery.flipper.js
@@ -6,19 +6,13 @@
  */
 ;(function($, window, document, undefined) {
 
-    var pluginName = 'flipper',
-        defaults = {
-            rotationType: 'right',
-            depth: 1000,
-            speed: 0.5
-        };
-
     $.widget('cjroe.flipper', {
 
         options: {
             _classPrefix: '_flipper',
+            _faces: ['front', 'back'],
             // the type of rotation applied to the element (left, right, up, down, left-slide, right-slide)
-            rotationType: 'right',
+            rotationType: 'left',
             // the css perspective applied to the container of the rotating element
             depth: 1000,
             // the speed of the rotation
@@ -40,15 +34,44 @@
          * </section>
          */
         _create: function () {
+            this._defaults = this.options;
+
             this.element.addClass('flipper-container');
             this.flipperInside = this.element.find('#flipper-inside');
-            this.front = this.flipperInside.children().eq(0).addClass(this._prefix('front'));
-            this.back = this.flipperInside.children().eq(1).addClass(this._prefix('back'));
+
+            // create faces
+            this.flipperInside.children().each(function (i, el) {
+                this[this.options._faces[i]] = $(el).addClass(this._prefix(this.options._faces[i]));
+            }.bind(this));
 
             if ($.type(this.options.rotationType) === 'string') {
                 this.element.addClass(this._prefix(this.options.rotationType));
             }
 
+            this._setDepth();
+
+            this._setSpeed();
+        },
+
+        _setSpeed: function (speed) {
+            if ($.type(this.options.speed) === 'number') {
+                var _speed = parseFloat(this.options.speed);
+                if (speed || speed > 0.1) {
+                    if (speed > 2) {
+                        _speed = 2.0;
+                    } else if (speed < 0.1) {
+                        _speed = 0.1;
+                    } else {
+                        _speed = speed;
+                    }
+                }
+                this.flipperInside.css({
+                    transition: 'all ' + _speed + 's ease'
+                });
+            }
+        },
+
+        _setDepth: function (v) {
             if ($.type(this.options.depth) === 'number'){
                 var depth = parseInt(this.options.depth);
                 if (depth > 2000) {
@@ -57,30 +80,45 @@
                     depth = 100;
                 }
 
+                if (v) {
+                    depth = v;
+                }
                 this.element.css({
                     perspective: depth + 'px'
                 });
             } else {
                 throw new Error(this.options.depth + ' must be typeof Number');
             }
-
-            if ($.type(this.options.speed) === 'number') {
-                var speed = parseFloat(this.options.speed);
-                this.flipperInside.css({
-                    transition: 'all ' + speed + 's ease'
-                });
-            }
         },
 
         _setOption: function (k, v) {
+            
+            switch(k) {
+                case 'rotationType':
+                    if ($.type(v) == 'string') {
+                        this.element.switchClass(this._prefix(this.options.rotationType), this._prefix(v));
+                    }
+                    break;
+                case 'depth':
+                    var v = depth = parseInt(v);
+                    this._setDepth(depth);
+                    break;
+                case 'speed':
+                    var v = speed = parseFloat(v);
+                    this._setSpeed(speed);
+                default: '';
+            }
+            this._update(k, v);
+        },
+
+        _update: function (k, v) {
             this.options[k] = v;
-            this._update();
         },
 
         _destroy: function () {
             this.element.removeClass('flipper-container');
-            this.front.removeClass(this._prefix('front'));
-            this.back.removeClass(this._prefix('back'));
+            this.front.removeClass(this._prefix(this.options._faces[0]));
+            this.back.removeClass(this._prefix(this.options._faces[1]));
         }
 
     });
