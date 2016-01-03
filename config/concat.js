b5f3450b70788f3d7cad38111c172caada85e5e7
Cameron J Roe <cameronjroe@gmail.com>
2016-01-03 14:09:05 -0500

    rm old build

--- config/concat.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  options: {
-    stripBanners: true
-  },
-  dist: {
-    src: ['src/jquery.<%= pkg.name %>.js'],
-    dest: 'dist/jquery.<%= pkg.name %>.js'
-  }
-};
--- config/uglify.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  options: {
-    banner: '<%= banner %>'
-  },
-  dist: {
-    src: '<%= concat.dist.dest %>',
-    dest: 'dist/jquery.<%= pkg.name %>.min.js'
-  }
-};
